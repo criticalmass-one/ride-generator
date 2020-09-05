672fc8c1041b923a710752e0d57d6de29d04a373
Malte Hübner <maltehuebner@gmx.org>
2020-09-05 18:42:01 +0200

    composer require nelmio/api-doc-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
