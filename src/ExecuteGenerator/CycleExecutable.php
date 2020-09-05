b6911f153e67793fa5e1abfb780e4ef717dd5ea4
Malte Hübner <maltehuebner@gmx.org>
2020-09-05 11:32:48 +0200

    Extend CycleExecuteable.

--- src/ExecuteGenerator/CycleExecutable.php
+++ src/ExecuteGenerator/CycleExecutable.php
@@ -2,6 +2,8 @@
 
 namespace App\ExecuteGenerator;
 
+use App\Model\City;
+use App\Model\CityCycle;
 use App\Validator\Constraint\ExecutorDateTime;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -10,6 +12,10 @@ use Symfony\Component\Validator\Constraints as Assert;
  */
 class CycleExecutable
 {
+    protected ?City $city = null;
+
+    protected ?CityCycle $cityCycle = null;
+
     /**
      * @var \DateTime $fromDate
      * @Assert\GreaterThanOrEqual("1992-09-01", message="Vor September 1992 können keine Touren angelegt werden — das ist übrigens das Datum der allerersten Critical Mass in San Francisco.")
@@ -48,6 +54,30 @@ class CycleExecutable
     public function setUntilDate(\DateTime $untilDate = null): self
     {
         $this->untilDate = $untilDate;
+
+        return $this;
+    }
+
+    public function getCity(): ?City
+    {
+        return $this->city;
+    }
+
+    public function setCity(?City $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getCityCycle(): ?CityCycle
+    {
+        return $this->cityCycle;
+    }
+
+    public function setCityCycle(?CityCycle $cityCycle): self
+    {
+        $this->cityCycle = $cityCycle;
 
         return $this;
     }
