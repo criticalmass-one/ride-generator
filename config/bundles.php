ec5052b3051759c8c9416b0cabe9522d837b2169
Malte Hübner <maltehuebner@gmx.org>
2020-09-05 18:43:41 +0200

    composer require symfony/maker-bundle --dev

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
