7e8a0ce40b82c88f96ab7d0e7768b4f4782266e0
Malte Hübner <maltehuebner@gmx.org>
2022-05-03 02:09:04 +0200

    Update to symfony 6, adjust api.

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -5,41 +5,37 @@ namespace App\Controller;
 use App\CycleFetcher\CycleFetcherInterface;
 use App\ExecuteGenerator\CycleExecutable;
 use App\RideGenerator\CycleRideGeneratorInterface;
-use App\RideGenerator\RideGeneratorInterface;
-use Carbon\Carbon;
 use Carbon\CarbonInterval;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Swagger\Annotations as SWG;
+use OpenApi\Annotations as OA;
 use Nelmio\ApiDocBundle\Annotation\Model;
 
 class ApiController extends AbstractController
 {
     /**
      * @Route("/api/preview", name="api_preview", methods={"POST"})
-     * @SWG\Response(
+     * @OA\Response(
      *     response=200,
      *     description="Returns a preview of the rides to be created by the provided CycleExecuteable.",
-     *     @SWG\Schema(
+     *     @OA\Schema(
      *         type="array",
-     *         @SWG\Items(ref=@Model(type=App\Model\Ride::class, groups={"full"}))
+     *         @OA\Items(ref=@Model(type=App\Model\Ride::class, groups={"full"}))
      *     )
      * )
-     * @SWG\Parameter(
+     * @OA\Parameter(
      *     name="body",
      *     in="body",
-     *     type="string",
      *     description="The CycleExecuteable to create rides.",
-     *     @SWG\Schema(
+     *     @OA\Schema(
      *         type="object",
-     *         @SWG\Items(ref=@Model(type=CycleExecuteable::class, groups={"full"}))
+     *         @OA\Items(ref=@Model(type=CycleExecuteable::class, groups={"full"}))
      *     )
      * )
-     * @SWG\Tag(name="Ride Generator")
+     * @OA\Tag(name="Ride Generator")
      */
     public function preview(Request $request, SerializerInterface $serializer, CycleFetcherInterface $cycleFetcher, CycleRideGeneratorInterface $rideGenerator): Response
     {
